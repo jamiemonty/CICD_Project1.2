0137eb8f502f404f90012a2eadaf000c90e0b7d3
Jamie <G00419525@atu.ie>
2025-01-04 23:09:35 +0000

    added some code so react could communicate with the api.

--- src/main/java/ie/atu/experiencelevel/ExperienceLevel.java
+++ src/main/java/ie/atu/experiencelevel/ExperienceLevel.java
@@ -6,14 +6,36 @@ import lombok.*;
 
 @Entity
 @Table(name = "experience_level")
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 public class ExperienceLevel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "exp_id") // Specifies the column name in MySQL
     private Long expId;
 
+    @Column(name = "level") // Assuming level is the name of the column
     private String level;
+
+    public ExperienceLevel() {}
+
+    public ExperienceLevel(Long id, String level) {
+        this.expId = id;
+        this.level = level;
+    }
+
+    public Long getId() {
+        return expId;
+    }
+
+    public void setId(Long id) {
+        this.expId = id;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
 }
